2513cf231e5d34ac87ee9b4136df8d8e5169d045
Badar Shahbaz <badarbutt121@gmail.com>
2021-07-18 00:08:49 +0500

    JS improvements

--- assets/wwl-script.js
+++ assets/wwl-script.js
@@ -1,12 +1,13 @@
 window.addEventListener('DOMContentLoaded', (e) => {
 	let loggedIn = document.querySelector('body').classList.contains('logged-in') ? true : false;
+	if(loggedIn) {
 
-	if(document.querySelector('.wishlist-toggle')){
-		document.querySelector('.wishlist-toggle').addEventListener('click', (e) => {
-			e.preventDefault();
-			let productId = e.target.getAttribute("data-product");
-			
-			if(loggedIn) {
+		// When user will click on add to wishlist button.
+		if(document.querySelector('.wishlist-toggle')){
+			document.querySelector('.wishlist-toggle').addEventListener('click', (e) => {
+				e.preventDefault();
+				let productId = e.target.getAttribute("data-product");
+				
 				let formData = new FormData();
 				formData.append('action', 'sbwl_add_to_wishlist');
 				formData.append('user_id', opts.userId);
@@ -19,23 +20,24 @@ window.addEventListener('DOMContentLoaded', (e) => {
 						console.log('working');
 					}
 				});
-			}
-		});
-	}
+			});
+		}
 
-	if(document.querySelector('.wishlist-toggle')){
-		document.querySelector('.wishlist-toggle').addEventListener('mouseup',function() {
-			e.preventDefault();
-			document.querySelector('.wl-new-added').classList.remove('wwl-hide');
-		});
-	}
+		// After add to wishlist button is clicked.
+		if(document.querySelector('.wishlist-toggle')){
+			document.querySelector('.wishlist-toggle').addEventListener('mouseup',function() {
+				e.preventDefault();
+				document.querySelector('.wl-new-added').classList.remove('wwl-hide');
+			});
+		}
 
-	if(document.querySelector('.wwl-rfc-btn')){
-		document.querySelector('.wwl-rfc-btn').addEventListener('click', (e) => {
-			e.preventDefault();
-			let productId = e.target.getAttribute("data-product");
-			
-			if(loggedIn) {
+		// When user will click add on remove from wishlist button/icon.
+		if(document.querySelector('.wwl-rfc-btn')){
+			let wowlBtns = document.querySelectorAll('.wwl-rfc-btn');
+			for(var i = 0; i<wowlBtns.length; i++) {
+				e.preventDefault();
+				let productId = e.target.getAttribute("data-product");
+				
 				let formData = new FormData();
 				formData.append('action', 'sbwl_remove_from_wishlist');
 				formData.append('user_id', opts.userId);
@@ -49,7 +51,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
 					}
 				});
 			}
-		});
+		}
 	}
 	
 });
